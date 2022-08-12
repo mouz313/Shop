3fda9b5518b5c95e22471a205c2b4355b0ff2a63
Usman Saeed <pakistan1122@live.com>
2022-08-12 17:06:45 +0500

    set admin login page

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -192,12 +192,12 @@
         <footer class="footer mt-auto">
             <div class="copyright bg-white">
               <p>
-                &copy; <span id="copy-year">2019</span> Copyright Sleek Dashboard Bootstrap Template by
+                &copy; <span id="copy-year">2022</span> Copyright Dashboard Bootstrap Template by
                 <a
                   class="text-primary"
-                  href="http://www.iamabdus.com/"
+                  href="http://www.root4tech.com/"
                   target="_blank"
-                  >Abdus</a
+                  >Root4tech</a
                 >.
               </p>
             </div>
